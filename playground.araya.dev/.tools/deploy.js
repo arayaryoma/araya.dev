51c7caad50495fd3787f1313819a80bdfb609764
arayaryoma <r1o.ryoma.abe@gmail.com>
2020-07-27 18:12:40 +0900

    :sparkles: Implement excluding fils and dirs

--- playground.araya.dev/.tools/deploy.js
+++ playground.araya.dev/.tools/deploy.js
@@ -11,15 +11,18 @@ const readIgnoredPaths = async () => {
 const run = async () => {
   const target = process.argv[2];
   const ignoredPaths = await readIgnoredPaths();
-  const excludes = ignoredPaths.map(
-    (ignored) => `--exclude="${path.resolve(process.cwd(), target, ignored)}"`
-  ).join(" ");
+  const excludes = ignoredPaths
+    .map(
+      // (ignored) => `--filter "- ${path.relative(path.resolve(__dirname), path.resolve(process.cwd(), target, ignored))}"`
+      (ignored) => `--filter "- ${ignored}"`
+    )
+    .join(" ");
   const remoteDir = path.parse(process.cwd()).name;
   const key = path.resolve(process.env.HOME, ".ssh/araya.dev.pem");
-  const cmd = `rsync --delete -r -e "ssh -i ${key}" ${path.resolve(
+  const cmd = `rsync -avrh --delete -e "ssh -i ${key}" ${excludes} ${path.resolve(
     process.cwd(),
     target
-  )}/ ${excludes} ubuntu@${SERVER_IP}:/var/www/araya.dev/playground.araya.dev/${remoteDir}`;
+  )}/ ubuntu@${SERVER_IP}:/var/www/araya.dev/playground.araya.dev/${remoteDir}`;
   console.log(cmd);
   exec(cmd, (err, stdout, stderr) => {
     if (err) {

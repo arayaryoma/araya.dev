049ca9fc0a175898c56e55a02df913357aef6793
arayaryoma <r1o.ryoma.abe@gmail.com>
2021-03-08 00:33:51 +0900

    :sparkles: Render images' size

--- www.araya.dev/assets/main.js
+++ www.araya.dev/assets/main.js
@@ -1,9 +1,24 @@
 const $ = (s) => document.querySelector(s);
 const $$ = (s) => Array.from(document.querySelectorAll(s));
 
-const images = $$(".item img");
-images.forEach((image) => {
-  image.addEventListener("load", () => {
-    console.log(image.naturalWidth, image.naturalHeight);
-  });
-});
+class ImageInfo extends HTMLElement {
+  constructor() {
+    super();
+    if (this.shadowRoot) {
+      console.log("DSD  exists");
+    } else {
+      const template = this.querySelector("#image-info-template");
+      const shadow = this.attachShadow({ mode: "open" });
+      shadow.appendChild(template.content.cloneNode(true));
+    }
+
+    const valueEl = this.shadowRoot.querySelector("[data-size-value]");
+    const img = this.querySelector("img");
+
+    img.addEventListener("load", () => {
+      valueEl.innerText = `${img.naturalWidth} x ${img.naturalHeight}`;
+    });
+  }
+}
+
+customElements.define("image-info", ImageInfo);

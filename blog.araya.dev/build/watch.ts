a5d17eec1d6f44db1b484f1ec3c2161b262a87ee
arayaryoma <r1o.ryoma.abe@gmail.com>
2021-03-11 03:56:20 +0900

    :wrench: watch build

---
+++ blog.araya.dev/build/watch.ts
@@ -0,0 +1,8 @@
+import { CWD } from "./io.ts";
+
+await import("./build.tsx");
+
+const watcher = Deno.watchFs(`${CWD}/src`);
+for await (const event of watcher) {
+  await import("./build.tsx");
+}

dd4708b0fe7c4b590f2709abaa9d6debac01e578
arayaryoma <r1o.ryoma.abe@gmail.com>
2021-04-04 23:53:37 +0900

    :shower: comment out

--- blog.araya.dev/build/og-image.ts
+++ blog.araya.dev/build/og-image.ts
@@ -1,11 +1,9 @@
-import { Canvas } from "../deps.ts";
+// import { writeFile } from "./io.ts";
 
-import { writeFile } from "./io.ts";
+// const canvas = Canvas.MakeCanvas(200, 200);
+// const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-const canvas = Canvas.MakeCanvas(200, 200);
-const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+// ctx.fillStyle = "red";
+// ctx.fillRect(10, 10, 200 - 20, 200 - 20);
 
-ctx.fillStyle = "red";
-ctx.fillRect(10, 10, 200 - 20, 200 - 20);
-
-await writeFile("./canvas.png", canvas.toBuffer());
+// await writeFile("./canvas.png", canvas.toBuffer());

75dccf4df7e44cd1ecfada147b12e551c89ff622
arayaryoma <r1o.ryoma.abe@gmail.com>
2021-04-04 15:41:46 +0900

    :shower: Comment out

--- blog.araya.dev/build/watch.ts
+++ blog.araya.dev/build/watch.ts
@@ -1,17 +1,17 @@
-import { CWD } from "./io.ts";
-import { build } from "./build.tsx";
+// import { CWD } from "./io.ts";
+// import { build } from "./build.tsx";
 
-const watcher = Deno.watchFs(`${CWD}/src`);
-for await (const event of watcher) {
-  console.log(event.kind, event.paths);
-  await build();
-  await sleep(100);
-}
+// const watcher = Deno.watchFs(`${CWD}/src`);
+// for await (const event of watcher) {
+//   console.log(event.kind, event.paths);
+//   await build();
+//   await sleep(100);
+// }
 
-function sleep(ms: number): Promise<void> {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(undefined);
-    }, ms);
-  });
-}
+// function sleep(ms: number): Promise<void> {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(undefined);
+//     }, ms);
+//   });
+// }

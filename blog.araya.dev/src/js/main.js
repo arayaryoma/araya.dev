3dd5e9c9c18080f4f4ed6928f7f3f423596f7a3d
arayaryoma <r1o.ryoma.abe@gmail.com>
2021-05-04 00:22:53 +0900

    :nail_care: Set html attribute to change color-scheme

---
+++ blog.araya.dev/src/js/main.js
@@ -0,0 +1,14 @@
+
+const PREFERRED_COLOR_SCHEME = "preferred-color-scheme";
+
+function detectPreferredColorScheme() {
+  const userSetting = localStorage.getItem(PREFERRED_COLOR_SCHEME);
+  if (userSetting) return userSetting ?? "light";
+  const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  return isDark ? "dark" : "light";
+}
+
+const colorScheme = detectPreferredColorScheme();
+
+const html = document.querySelector("html");
+html?.setAttribute("data-color-scheme", colorScheme);

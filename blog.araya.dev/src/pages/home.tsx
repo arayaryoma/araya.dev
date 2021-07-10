0f8440a5e50efefb7a1408de171ec045d7f7e2f9
Ryoma Abe <ryoma.abe@nex.nikkei.com>
2021-07-11 00:34:27 +0900

    :shower: Refactor class names

--- blog.araya.dev/src/pages/home.tsx
+++ blog.araya.dev/src/pages/home.tsx
@@ -16,21 +16,21 @@ export const Home = (props: Props) => {
     <main>
       <div className="container">
         <h1 className="index-page-title heading__lined">Posts</h1>
-        <ul className="posts-list__root posts-list--list">
+        <ul className="posts-list__root posts-list__list">
           {props.posts.map((post) => (
-            <li className="posts-list--item" key={post.url}>
+            <li className="posts-list__item" key={post.url}>
               <a href={post.url}>
-                <article className="posts-list--item-content">
+                <article className="posts-list-item__content">
                   <img
                     src="/assets/images/default-thumbnail.svg"
                     alt="thumbnail"
-                    className="posts-list--item-thumbnail"
+                    className="posts-list-item__thumbnail"
                     loading="lazy"
                   />
-                  <time className="posts-list--item-date" dateTime={post.date}>
+                  <time className="posts-list-item__date" dateTime={post.date}>
                     {post.date}
                   </time>
-                  <h2 className="posts-list--item-title">{post.title}</h2>
+                  <h2 className="posts-list-item__title">{post.title}</h2>
                 </article>
               </a>
             </li>

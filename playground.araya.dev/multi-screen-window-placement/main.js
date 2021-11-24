d0fed76602b7c0b5f0d7afdf5796e160c81528b5
araya <r1o.ryoma.abe@gmail.com>
2021-11-24 16:31:00 +0900

    Add an example of Multi-Screen Window Placement

---
+++ playground.araya.dev/multi-screen-window-placement/main.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector("#toggleBtn");
+const mainContent = document.querySelector("#mainContent");
+const screensNumEl = document.querySelector("#screensNumMessage");
+
+let screensNumMessage = "";
+// https://github.com/webscreens/window-placement/blob/main/EXPLAINER.md#add-screenisextended-to-expose-the-presence-of-extended-screen-areas
+if (window.screen.isExtended === undefined) {
+  screensNumMessage = "Your browser doesn't support `Screen.isExtended `";
+} else if (window.screen.isExtended === false) {
+  screensNumMessage = "You have a screen";
+} else if (window.screen.isExtended === true) {
+  screensNumMessage = "You have 2 or more screens";
+}
+screensNumEl.innerText = screensNumMessage;
+
+btn.addEventListener("click", () => {
+  if (!document.fullscreenElement) {
+    mainContent.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});

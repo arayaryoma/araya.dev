94260b8bfb882c4ee25dd36b9fde04700c4c9faf
arayaryoma <r1o.ryoma.abe@gmail.com>
2021-11-24 21:18:44 +0900

    Add window.getScreenDetails() sample

--- playground.araya.dev/multi-screen-window-placement/main.js
+++ playground.araya.dev/multi-screen-window-placement/main.js
@@ -1,6 +1,7 @@
-const btn = document.querySelector("#toggleBtn");
+const toggleFullScreenBtn = document.querySelector("#toggleBtn");
 const mainContent = document.querySelector("#mainContent");
 const screensNumEl = document.querySelector("#screensNumMessage");
+const getScreenDetailBtn = document.querySelector("#getScreenDetailBtn");
 
 let screensNumMessage = "";
 // https://github.com/webscreens/window-placement/blob/main/EXPLAINER.md#add-screenisextended-to-expose-the-presence-of-extended-screen-areas
@@ -13,7 +14,7 @@ if (window.screen.isExtended === undefined) {
 }
 screensNumEl.innerText = screensNumMessage;
 
-btn.addEventListener("click", () => {
+toggleFullScreenBtn.addEventListener("click", () => {
   if (!document.fullscreenElement) {
     mainContent.requestFullscreen();
   } else {
@@ -25,3 +26,11 @@ window.screen.addEventListener("change", (e) => {
   console.log("window.screen has changed: ", e);
 });
 
+getScreenDetailBtn.addEventListener("click", async () => {
+  try {
+    const screenDetails = await window.getScreenDetails();
+    console.log(screenDetails);
+  } catch (e) {
+    console.error("Failed to get screen details: ", e);
+  }
+});

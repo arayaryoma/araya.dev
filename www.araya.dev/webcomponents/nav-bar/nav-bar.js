ddfba17fdbb1ef9fd81d37df9aa03768127fb530
araya <r1o.ryoma.abe@gmail.com>
2022-05-08 18:01:13 +0900

    NavBar as WebComponents with DSD

--- www.araya.dev/webcomponents/nav-bar/nav-bar.js
+++ www.araya.dev/webcomponents/nav-bar/nav-bar.js
@@ -1,44 +1,15 @@
-const navItems = [
-  {
-    name: "About",
-    url: "#about",
-  },
-  {
-    name: "Job History",
-    url: "#job-history",
-  },
-];
 export class NavBar extends HTMLElement {
   constructor() {
     super();
-  }
+    const internals = this.attachInternals?.();
 
-  connectedCallback() {
-    const nav = document.createElement("nav");
-    nav.classList.add("nav");
-    const navList = document.createElement("ul");
-    navList.classList.add("nav-list");
-    for (const item of navItems) {
-      const itemEl = document.createElement("li");
-      itemEl.classList.add("nav-item");
-      const anchor = document.createElement("a");
-      anchor.setAttribute("href", item.url);
-      anchor.innerText = item.name;
-      itemEl.appendChild(anchor);
-      navList.appendChild(itemEl);
+    // check for a Declarative Shadow Root:
+    let shadow = internals?.shadowRoot;
+    if (!shadow) {
+      const template = this.querySelector("template[shadowroot]");
+      const mode = template.getAttribute("shadowroot");
+      shadow = this.attachShadow({ mode });
+      shadow.appendChild(template.content.cloneNode(true));
     }
-    nav.appendChild(navList);
-
-    const iconContainer = document.createElement("div");
-    iconContainer.classList.add("icon");
-
-    const iconImg = document.createElement("img");
-    iconImg.setAttribute("src", "/assets/white-cat.svg");
-    iconImg.setAttribute("alt", "icon");
-
-    iconContainer.appendChild(iconImg);
-
-    nav.appendChild(iconContainer);
-    this.appendChild(nav);
   }
 }

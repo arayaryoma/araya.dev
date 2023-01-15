f2fa5e19c5f3d5ffd1470ee620105ceab98246b5
Ryoma Abe <ryoma.abe@nex.nikkei.com>
2023-01-15 10:53:45 +0900

    Remove unused build scripts

--- blog.araya.dev/build/hash.ts
+++
@@ -1,9 +0,0 @@
-import { createHash } from "crypto";
-
-const hash = createHash("md5");
-
-export const contentHash = (content: string) => {
-  return createHash("md5")
-    .update(content)
-    .digest("hex");
-};

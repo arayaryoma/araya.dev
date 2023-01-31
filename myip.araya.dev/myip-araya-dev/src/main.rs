eda4038bc9444e81ca4573169a7d4828d8a74cab
araya <r1o.ryoma.abe@gmail.com>
2023-01-31 13:04:45 +0000

    Read HTTP request

--- myip.araya.dev/myip-araya-dev/src/main.rs
+++ myip.araya.dev/myip-araya-dev/src/main.rs
@@ -1,5 +1,6 @@
 use std::{
-    net::{TcpListener}
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
 };
 
 fn main() {
@@ -7,6 +8,17 @@ fn main() {
     for stream in listener.incoming() {
             let stream = stream.unwrap();
 
-            println!("Connection established");
+            handle_connection(stream)
+
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader.lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }

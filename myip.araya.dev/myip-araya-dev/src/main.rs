1da7264211e38d8bbf6b598bf73c6d793e008d09
araya <r1o.ryoma.abe@gmail.com>
2023-01-31 13:07:06 +0000

    Return OK response

--- myip.araya.dev/myip-araya-dev/src/main.rs
+++ myip.araya.dev/myip-araya-dev/src/main.rs
@@ -6,19 +6,23 @@ use std::{
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3001").unwrap();
     for stream in listener.incoming() {
-            let stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-            handle_connection(stream)
-
+        handle_connection(stream)
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader.lines()
+    let http_request: Vec<_> = buf_reader
+        .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
     println!("Request: {:#?}", http_request);
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }

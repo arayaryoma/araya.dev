34c1997f833010a7811ebb90c5da672fabec9076
araya <araya@araya.dev>
2023-02-19 23:06:15 +0900

    Fix broken thumbnails
    This reverts commit 4d489120205b9af2fcd190b1f8e3625c4ed8e567.

--- blog.araya.dev/astro.config.mjs
+++ blog.araya.dev/astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
-  trailingSlash: 'always',
-  integrations: [image()]
+	trailingSlash: 'always',
 });

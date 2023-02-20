a110fe81fb4555f6314d94d535d8368e01997114
araya <araya@araya.dev>
2023-02-20 14:11:56 +0900

    Setup @astrojs/image

--- blog.araya.dev/astro.config.mjs
+++ blog.araya.dev/astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-	trailingSlash: 'always',
-	integrations: [mdx()],
+  trailingSlash: 'always',
+  integrations: [mdx(), image()]
 });

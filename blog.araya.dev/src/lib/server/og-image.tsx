5788d5b3d0ed1daf0b49e1d07d5a0b693544f805
araya <araya@araya.dev>
2023-08-21 00:48:53 +0900

    Aligh text and improve visual

--- blog.araya.dev/src/lib/server/og-image.tsx
+++ blog.araya.dev/src/lib/server/og-image.tsx
@@ -12,25 +12,41 @@ type OgImageParams = {
 export async function generateOgImage({
   title,
 }: OgImageParams): Promise<Buffer> {
-  const svg = await satori(<h1 style={{ fontSize: "44px" }}>{title}</h1>, {
-    width: 800,
-    height: 400,
-    fonts: [
-      {
-        name: "ZenMaruGothic",
-        data: fs.readFileSync(
-          path.resolve(
-            __dirname,
-            "../../../public/assets/fonts/ZenMaruGothic-Bold.ttf"
-          )
-        ),
-        weight: 700,
-        style: "normal",
-        lang: "ja-JP",
-      },
-    ],
-    embedFont: true,
-  });
+  const svg = await satori(
+    <div
+      style={{
+        display: "flex",
+        backgroundColor: "#f8f8f8",
+        color: "#6450a1",
+        height: "100%",
+        flexDirection: "column",
+      }}
+    >
+      <h1 style={{ fontSize: "44px" }}>{title}</h1>
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <span>blog.araya.dev</span>
+      </div>
+    </div>,
+    {
+      width: 800,
+      height: 400,
+      fonts: [
+        {
+          name: "ZenMaruGothic",
+          data: fs.readFileSync(
+            path.resolve(
+              __dirname,
+              "../../../public/assets/fonts/ZenMaruGothic-Bold.ttf"
+            )
+          ),
+          weight: 700,
+          style: "normal",
+          lang: "ja-JP",
+        },
+      ],
+      embedFont: true,
+    }
+  );
 
   return sharp(Buffer.from(svg)).png().toBuffer();
 }
